ff11075db2cdf834aa1186a3a6c4a05082d66d11
yasseralshimmary79 <alshimmaryyasser@gmail.com>
2024-02-14 15:40:39 +0000

    calculate and display largest and smallest numbers

--- average.py
+++ average.py
@@ -6,6 +6,11 @@ if len(numbers_list) == 0:
     print("No numbers provided.")
 else:
     average = sum(numbers_list) / len(numbers_list)
+    smallest = min(numbers_list)
+    largest = max(numbers_list)
+    
     print("Average:", average)
+    print("Smallest number:", smallest)
+    print("Largest number:", largest)
 
 
